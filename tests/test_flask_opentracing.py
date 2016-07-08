a3e33bbb2f34aafdc0e6827568309d788809e7d8
= <kacamenzind@gmail.com>
2016-07-07 17:44:20 -0700

    Add additional tests

--- tests/test_flask_opentracing.py
+++ tests/test_flask_opentracing.py
@@ -9,11 +9,9 @@ app = Flask(__name__)
 test_app = app.test_client()
 
 empty_tracer = opentracing.Tracer()
-tracer = FlaskTracer(empty_tracer, True, app, ['url', 'url_rule'])
+tracer_with_attributes = FlaskTracer(empty_tracer, True, app, ['url', 'url_rule'])
 tracer_without_attributes = FlaskTracer(empty_tracer, True, app)
-
-print "HI"
-assert True 
+tracer = FlaskTracer(empty_tracer)
 
 @app.route('/test')
 def check_test_works():
@@ -29,27 +27,42 @@ def test_on():
 def test_span_creation():
     with app.test_request_context('/test'):
         app.preprocess_request()
-        assert tracer.get_span(request)
+        assert tracer_with_attributes.get_span(request)
+        assert tracer_without_attributes.get_span(request)
         # clear current spans
-        span = tracer.get_span(request)
+        span = tracer_with_attributes.get_span(request)
+        span2 = tracer_with_attributes.get_span(request)
         span.finish()
-        tracer._current_spans = {}
+        span2.finish()
+        tracer_with_attributes._current_spans = {}
+        tracer_without_attributes._current_spans = {}
 
 def test_span_deletion():
-    assert not tracer._current_spans
+    assert not tracer_with_attributes._current_spans
+    assert not tracer_without_attributes._current_spans
     test_app.get('/test')
-    assert not tracer._current_spans
+    assert not tracer_with_attributes._current_spans
+    assert not tracer_without_attributes._current_spans
 
-def test_spans_unique():
+def test_requests_distinct():
     with app.test_request_context('/test'):
         app.preprocess_request()
-    with app.test_request_context('/another_test'):
+    with app.test_request_context('/test'):
         app.preprocess_request()
-        second_span = tracer.get_span(request)
-        assert tracer.get_span(request) == second_span
-        second_span = tracer._current_spans.pop(request)
-        second_span.finish()
-        assert not tracer.get_span(request)
-        for req in tracer._current_spans:
-            tracer._current_spans[req].finish()
-    tracer._current_spans = {}
+        span = tracer_with_attributes._current_spans.pop(request)
+        span2 = tracer_without_attributes._current_spans.pop(request)
+        assert span
+        assert span2
+        span.finish()
+        span2.finish()
+        assert not tracer_with_attributes.get_span(request)
+        assert not tracer_without_attributes.get_span(request)
+    # clear current spans
+    for req in tracer_with_attributes._current_spans:
+        tracer_with_attributes._current_spans[req].finish()
+    tracer_with_attributes._current_spans = {}    
+    for req in tracer_without_attributes._current_spans:
+        tracer_without_attributes._current_spans[req].finish()
+    tracer_without_attributes._current_spans = {}
+
+# TODO: Tests for inject/extract; wait until new opentracing is on pypi
